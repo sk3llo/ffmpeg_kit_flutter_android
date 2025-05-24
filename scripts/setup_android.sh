6e0d398d5ec0ca5a71c365fbe1a4c6b6db9ca995
CoconutJohnny <kapraton@gmail.com>
2025-05-24 10:51:46 -0400

    - Version 1.4.0 - Custom Android implementation by using `jniLibs` and direct cpp bindings

--- scripts/setup_android.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Android AAR 下载
-ANDROID_URL="https://github.com/sk3llo/ffmpeg_kit_flutter/releases/download/6.0.2/ffmpeg-kit-full-gpl-6.0.LTS.aar"
-mkdir -p libs
-curl -L $ANDROID_URL -o libs/com.arthenica.ffmpegkit-flutter-6.0.aar
