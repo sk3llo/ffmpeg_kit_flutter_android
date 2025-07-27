3e10905697ac1b6537cecde3c4f4c294dff43f36
CoconutJohnny <kapraton@gmail.com>
2025-07-27 13:29:25 -0400

    ## 1.0.0
    * Initial release
    * FFmpeg version 7.1.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,11 +14,11 @@ kotlin {
 
 // Publishing [com.vanniktech.maven.publish] config
 mavenPublishing {
-    coordinates("com.antonkarpenko", "ffmpeg-kit-https", "1.0.0")
+    coordinates("com.antonkarpenko", "ffmpeg-kit-https-gpl", "1.0.0")
 
     pom {
-        name.set("FFmpeg v7.1.1 HTTPS")
-        description.set("FFmpeg v7.1.1 HTTPS")
+        name.set("FFmpeg v7.1.1 HTTPS-GPL")
+        description.set("FFmpeg v7.1.1 HTTPS-GPL")
         inceptionYear.set("2025")
         url.set("https://github.com/sk3llo/ffmpeg-kit-flutter")
         licenses {
