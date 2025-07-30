814527849ddd1ca8a03b921b9e3b06d1e014d334
CoconutJohnny <kapraton@gmail.com>
2025-07-29 20:54:58 -0400

    ## 1.0.2
    * Downgraded Kotlin version from 2.2.0 to 1.8.22

--- android/CHANGELOG.md
+++ android/CHANGELOG.md
@@ -1,8 +1,3 @@
-## 1.0.1
-
-* Added missing `twolame` dependency
-* Fixed `__gxx_personality_v0` for `libilbc` library
-
 ## 1.0.0
 
 * Initial release
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,18 @@
 plugins {
     id("com.android.library") version "8.11.1"
-    id("org.jetbrains.kotlin.android") version "2.2.0"
+    id("org.jetbrains.kotlin.android") version "1.8.22"
     id("maven-publish")
     id("signing")
     id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
-kotlin {
-    compilerOptions {
-        optIn.add("kotlin.RequiresOptIn")
-    }
-}
-
 // Publishing [com.vanniktech.maven.publish] config
 mavenPublishing {
-    coordinates("com.antonkarpenko", "ffmpeg-kit-audio", "1.0.1")
+    coordinates("com.antonkarpenko", "ffmpeg-kit-video", "1.0.0")
 
     pom {
-        name.set("FFmpeg v7.1.1 Audio")
-        description.set("FFmpeg v7.1.1 Aduio")
+        name.set("FFmpeg v7.1.1 Video")
+        description.set("FFmpeg v7.1.1 Video")
         inceptionYear.set("2025")
         url.set("https://github.com/sk3llo/ffmpeg-kit-flutter")
         licenses {
@@ -52,7 +46,7 @@ mavenPublishing {
 android {
     namespace = "com.antonkarpenko"
     compileSdk = 36
-    version = "1.0.1"
+    version = "1.0.0"
 
     defaultConfig {
         minSdk = 24
