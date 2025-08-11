96fd2a18e7352fd4ced13fdad36515cc65eed08f
CoconutJohnny <kapraton@gmail.com>
2025-08-11 12:20:04 -0400

    * Added proguard-rules.pro to keep `ffmpeg` dependencies when minification is enabled * Upgraded `freetype` from **2.13.0** to **2.13.3** * Upgraded `harfbuzz` from **8.0.1** to **11.3.3** * Upgraded `fontconfig` from **2.16.2** to 2.17.1 * Added support for `harfbuzz` library in order to support `drawtext` filter * Fixed missing `libunibreak` for `libass.sh`

--- android/CHANGELOG.md
+++ android/CHANGELOG.md
@@ -1,3 +1,12 @@
+## 1.2.0
+
+* Added proguard-rules.pro to keep `ffmpeg` dependencies when minification is enabled
+* Upgraded `freetype` from **2.13.0** to **2.13.3**
+* Upgraded `harfbuzz` from **8.0.1** to **11.3.3**
+* Upgraded `fontconfig` from **2.16.2** to 2.17.1
+* Added support for `harfbuzz` library in order to support `drawtext` filter
+* Fixed missing `libunibreak` for `libass.sh`
+
 ## 1.1.0
 
 * Added support for hevc_mediacodec
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 // Publishing [com.vanniktech.maven.publish] config
 mavenPublishing {
-    coordinates("com.antonkarpenko", "ffmpeg-kit-full", "1.1.0")
+    coordinates("com.antonkarpenko", "ffmpeg-kit-full", "1.2.0")
 
     pom {
         name.set("FFmpeg v7.1.1 Full")
@@ -46,7 +46,7 @@ mavenPublishing {
 android {
     namespace = "com.antonkarpenko"
     compileSdk = 36
-    version = "1.1.0"
+    version = "1.2.0"
 
     defaultConfig {
         minSdk = 24
