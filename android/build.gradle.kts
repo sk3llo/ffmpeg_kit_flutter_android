b0e9e8d590626f010b28f20db43b905c87c095cd
CoconutJohnny <kapraton@gmail.com>
2025-10-25 12:04:11 +0200

    ## 2.0.0
    * Initial FFmpeg 8.0 release
    * Added proguard-rules.pro

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.11.1"
+    id("com.android.library") version "8.12.3"
     id("org.jetbrains.kotlin.android") version "1.8.22"
     id("maven-publish")
     id("signing")
@@ -8,11 +8,11 @@ plugins {
 
 // Publishing [com.vanniktech.maven.publish] config
 mavenPublishing {
-    coordinates("com.antonkarpenko", "ffmpeg-kit-full", "1.2.0")
+    coordinates("com.antonkarpenko", "ffmpeg-kit-full", "2.0.0")
 
     pom {
-        name.set("FFmpeg v7.1.1 Full")
-        description.set("FFmpeg v7.1.1 Full")
+        name.set("FFmpeg v8.0.0 Full")
+        description.set("FFmpeg v8.0.0 Full")
         inceptionYear.set("2025")
         url.set("https://github.com/sk3llo/ffmpeg-kit-flutter")
         licenses {
@@ -46,12 +46,21 @@ mavenPublishing {
 android {
     namespace = "com.antonkarpenko"
     compileSdk = 36
-    version = "1.2.0"
+    version = "2.0.0"
 
     defaultConfig {
         minSdk = 24
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
 
     compileOptions {
